f93b2131873906e3b0ff89632096b94ed6de0f68
MUmer04 <umer_1309e@aptechgdn.com>
2017-10-20 12:30:42 -0700

    done coding chapter 1 and 2 in assignment1

---
+++ assignment1/app.js
@@ -0,0 +1,10 @@
+
+//alert("Error! Please enter a valid password.");
+//alert("Welcome to JS Land \n Happy Coding");
+//alert("Welcome to JS Land");
+//alert("Happy Coding");
+//alert("Hello.. I can run JS through my web browser's console");
+var a, b, c;
+var one, One, one_1, _$dollor, _1$;
+//var 1num, #num, num&, o ne, 1_num; 
+alert("Rules for naming JS variables \n Variable names can only contain numbers, $ and _. For example : $my_1stVariable ");
